16ff7a639a56fb6c9d19fd2ea76d6f6a8f244333
Qi <zyfyus@outlook.com>
2020-06-03 18:34:36 +0800

    update with react lazy suspense

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import {hot} from 'react-hot-loader/root';
 
-import React, {Component} from 'react';
+import React, {Component, Suspense, lazy} from 'react';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
 import Home from './containers/Home';
@@ -11,6 +11,8 @@ import './App.css';
 
 import {themes, ThemeContext} from './theme-context';
 
+const Lazy = lazy(() => import('./components/Lazy'));
+
 class App extends Component {
   constructor() {
     super();
@@ -18,29 +20,35 @@ class App extends Component {
 
   render() {
     return (
-      <Router>
-        <div>
-          <nav className="crumbs">
-            <ol>
-              <li className="crumb">
-                <Link to="/">Home</Link>
-              </li>
-              <li className="crumb">
-                <Link to="/about">About</Link>
-              </li>
-              <li className="crumb">
-                <Link to="/topics">Topics</Link>
-              </li>
-            </ol>
-          </nav>
+      <>
+        <Router>
+          <div>
+            <nav className="crumbs">
+              <ol>
+                <li className="crumb">
+                  <Link to="/">Home</Link>
+                </li>
+                <li className="crumb">
+                  <Link to="/about">About</Link>
+                </li>
+                <li className="crumb">
+                  <Link to="/topics">Topics</Link>
+                </li>
+              </ol>
+            </nav>
 
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <ThemeContext.Provider value={themes.light}>
-            <Route path="/topics" component={Topics} />
-          </ThemeContext.Provider>
-        </div>
-      </Router>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <ThemeContext.Provider value={themes.light}>
+              <Route path="/topics" component={Topics} />
+            </ThemeContext.Provider>
+          </div>
+        </Router>
+
+        <Suspense fallback={<h1>Still Loading…</h1>}>
+          <Lazy />
+        </Suspense>
+      </>
     );
   }
 }
