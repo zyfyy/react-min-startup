14965cd389086134290bd69b60ddc13b22450ac8
zyfyy <zyfyus@outlook.com>
2021-09-04 19:09:29 +0800

    update

--- src/components/ThemeParagraph.tsx
+++ src/components/ThemeParagraph.tsx
@@ -2,15 +2,11 @@ import React from 'react';
 import { ThemeContext } from '../context/theme-context';
 
 export default class ThemeParagraph extends React.Component {
+  // 非函数式使用
   static contextType = ThemeContext;
   render() {
     let props = this.props;
     let theme = this.context;
-    return (
-      <p
-        {...props}
-        style={{ backgroundColor: theme.background, color: theme.foreground }}
-      />
-    );
+    return <p {...props} style={{ backgroundColor: theme.background, color: theme.foreground }} />;
   }
 }
--- src/modules/Basics/Context.tsx
+++ src/modules/Basics/Context.tsx
@@ -1,26 +1,18 @@
 import React, { FC, MouseEventHandler } from 'react';
-import {
-  ThemeContext,
-  themes,
-  ContextState,
-} from '../../context/theme-context';
+import { ThemeContext, themes, ContextState } from '../../context/theme-context';
 import ThemeParagraph from '../../components/ThemeParagraph';
 
 interface ThemeToggleButtonProps {
   toggleTheme: MouseEventHandler;
 }
 
-const ThemeToggleButton: FC<ThemeToggleButtonProps> = (
-  props: ThemeToggleButtonProps,
-) => {
+const ThemeToggleButton: FC<ThemeToggleButtonProps> = (props: ThemeToggleButtonProps) => {
   const { toggleTheme } = props;
   return (
     <ThemeContext.Consumer>
-      {(theme) => {
+      {theme => {
         return (
-          <button
-            onClick={toggleTheme}
-            style={{ backgroundColor: theme.background }}>
+          <button onClick={toggleTheme} style={{ backgroundColor: theme.background }}>
             Toggle Theme
           </button>
         );
@@ -30,7 +22,7 @@ const ThemeToggleButton: FC<ThemeToggleButtonProps> = (
 };
 
 const ChildContext = () => {
-  return <ThemeParagraph>child context</ThemeParagraph>;
+  return <ThemeParagraph>多个层级child context [非函数式使用，static声明式使用]</ThemeParagraph>;
 };
 
 class Context extends React.Component<{}, ContextState> {
@@ -43,7 +35,7 @@ class Context extends React.Component<{}, ContextState> {
   }
 
   toggleTheme = () => {
-    this.setState((state) => ({
+    this.setState(state => ({
       theme: state.theme === themes.dark ? themes.light : themes.dark,
     }));
   };
@@ -52,15 +44,18 @@ class Context extends React.Component<{}, ContextState> {
       <div className="context">
         <ThemeContext.Provider value={this.state.theme}>
           <button onClick={this.toggleTheme}>toggle</button>
-          <ThemeParagraph>我会变</ThemeParagraph>
+          <ThemeParagraph>一个层级 我会变</ThemeParagraph>
           <ChildContext></ChildContext>
+          <ThemeContext.Consumer>
+            {value => `使用Consumer函数式获取Context ${JSON.stringify(value)}`}
+          </ThemeContext.Consumer>
         </ThemeContext.Provider>
         {/* 没有Provider内，使用默认的值 */}
         <section>
-          <ThemeParagraph>我不会-变</ThemeParagraph>
+          <ThemeParagraph>没有在Provider下，使用默认值，不会变</ThemeParagraph>
         </section>
-
         <ThemeContext.Provider value={this.state.theme}>
+          <p>Multi place Provider works just fine: </p>
           <ThemeToggleButton toggleTheme={this.toggleTheme} />
         </ThemeContext.Provider>
       </div>
